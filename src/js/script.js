b298cf963f5d27dd798955da75890b9919e75611
Ula-Lysenko <highspirit@ukr.net>
2023-12-14 20:04:04 +0200

    hw 2 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// // Task #1
+//
+// let admin;
+// let name;
+//
+// name = 'John';
+//
+// admin = name;
+// alert (admin);
+
+
+// // Task #2
+//
+// const ourPlanetName = 'Earth';
+// let activeUserName;
+
+
+// Task #3
+
+let strAndNum = '10' + 10;
+console.log(strAndNum);
+console.log(typeof strAndNum);
