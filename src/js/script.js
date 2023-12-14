1120e52ec0d60c3dd9f5ebe91a250cca0ad56a77
Ula-Lysenko <highspirit@ukr.net>
2023-12-14 21:20:24 +0200

    hw 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let numberOne = +prompt ('Tell me the first number:');
+let numberTwo = +prompt ('Tell me the second number:');
+
+console.log(`Result: ${numberOne} + ${numberTwo} = ${numberOne + numberTwo}`);
+
+console.log(`Result: ${numberOne} - ${numberTwo} = ${numberOne - numberTwo}`);
+
+console.log(`Result: ${numberOne} * ${numberTwo} = ${numberOne * numberTwo}`);
+
+console.log(`Result: ${numberOne} / ${numberTwo} = ${numberOne / numberTwo}`);
+
