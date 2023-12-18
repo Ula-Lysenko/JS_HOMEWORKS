fd387250ce78c4b188505cf870967ef7b39f7770
Ula-Lysenko <highspirit@ukr.net>
2023-12-18 19:12:26 +0200

    hw 4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// 1
+
+const askWindowOne = prompt('Please, tell me your part of the story 1:');
+const askWindowTwo = prompt('Please, tell me your part of the story 2:');
+const askWindowThree = prompt('Please, tell me your part of the story 3:');
+
+alert(askWindowOne + ' ' + askWindowTwo + ' ' + askWindowThree + '!');
+//
+alert(`${askWindowOne} ${askWindowTwo} ${askWindowThree}!`);
+
+
+// 2
+
+const numbering = `98765`;
+console.log(`${numbering[0]} ${numbering[1]} ${numbering[2]} ${numbering[3]} ${numbering[4]}`);
+
+
+
+
+
+
