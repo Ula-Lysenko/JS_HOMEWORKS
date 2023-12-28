8c47cf4102abf7da623d7e97ae1c5ada94ddc52c
Ula-Lysenko <highspirit@ukr.net>
2023-12-28 16:19:23 +0200

    hw 12 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const askNumberOne = +prompt('Your first number:');
+
+const askNumberTwo = +prompt('Your second number:');
+
+
+alert(`${askNumberOne} + ${askNumberTwo} = ${askNumberOne + askNumberTwo}
+${askNumberOne} - ${askNumberTwo} = ${askNumberOne - askNumberTwo}
+${askNumberOne} * ${askNumberTwo} = ${askNumberOne * askNumberTwo}
+${askNumberOne} / ${askNumberTwo} = ${askNumberOne / askNumberTwo}`
+);
+
+// or, maybe
+
+alert(`${askNumberOne} + ${askNumberTwo} = ${askNumberOne + askNumberTwo}`);
+alert(`${askNumberOne} - ${askNumberTwo} = ${askNumberOne - askNumberTwo}`);
+alert(`${askNumberOne} * ${askNumberTwo} = ${askNumberOne * askNumberTwo}`);
+alert(`${askNumberOne} / ${askNumberTwo} = ${askNumberOne / askNumberTwo}`);
