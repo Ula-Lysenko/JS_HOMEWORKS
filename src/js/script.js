f39766b07c36449aad0c89d694ca2e35ba479fc8
Ula-Lysenko <highspirit@ukr.net>
2023-12-28 17:34:08 +0200

    hw 13 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const askOperation = prompt('What math operation do you need? (add, sub, multi, div)');
+
+const askNumberOne = +prompt('Your first number:');
+
+const askNumberTwo = +prompt('Your second number:');
+
+
+if (askOperation === 'add') alert(`${askNumberOne} + ${askNumberTwo} = ${askNumberOne + askNumberTwo}`);
+
+if (askOperation === 'sub') alert(`${askNumberOne} - ${askNumberTwo} = ${askNumberOne - askNumberTwo}`);
+
+if (askOperation === 'multi') alert(`${askNumberOne} * ${askNumberTwo} = ${askNumberOne * askNumberTwo}`);
+
+if (askOperation === 'div') alert(`${askNumberOne} / ${askNumberTwo} = ${askNumberOne / askNumberTwo}`);
+
