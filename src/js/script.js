23b0a1023052101bcefdfc215ce3a16316454eab
Ula-Lysenko <highspirit@ukr.net>
2023-12-31 19:12:48 +0200

    hw 15 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const askNumberOne = +prompt('Your first number:');
+
+const askNumberTwo = +prompt('Your second number:');
+
+const askNumberThree = +prompt('Your third number:');
+
+
+let answerArithmeticAverage = Math.round((askNumberOne + askNumberTwo + askNumberThree) / 3);
+
+alert(`WOW! You catch ${answerArithmeticAverage} snowflakes!`);
+
