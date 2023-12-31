467be2db3e20ef9b13ca677366d3ed3f6b22bc6e
Ula-Lysenko <highspirit@ukr.net>
2023-12-31 18:39:23 +0200

    hw 14 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const askNumberOfHours = +prompt('Please, enter your number of hours:');
+
+let answerNumberOfSeconds = askNumberOfHours * 3600;
+
+alert(`Greetings! You have ${answerNumberOfSeconds} seconds!`);
+
