32cc1236ca5b0eada444f7e1bebf36ca8efcde69
Ula-Lysenko <highspirit@ukr.net>
2024-01-05 22:41:28 +0200

    hw 17 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let numOrStr = prompt("Input number or string");
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        console.log("Ви скасували");
+        break;
+    case numOrStr === "":
+        console.log("Empty String");
+        break;
+    case isNaN(+numOrStr):
+        console.log("Number is Ba_NaN");
+        break;
+    default:
+        console.log("OK");
+}
