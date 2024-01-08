a95250663046f762dfedc5f958b59c7d9d5689dd
Ula-Lysenko <highspirit@ukr.net>
2024-01-08 17:28:04 +0200

    hw 16 updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,52 @@
 'use strict';
 
-const userAge = prompt('Your date of birth?');
+const userAge = prompt('Your year of birth?');
 
 const userCity = prompt('Your ciy?');
 
 const userSport = prompt('Your favorite sport?');
 
 
-let answerAge;
+const date = new Date();
+let age = 0;
 
 if (userAge) {
-    // alert(userAge);
-    answerAge = userAge.getFullYear() - userAge;
+    age = date.getFullYear() - userAge;
 } else {
-    alert ('So sorry, but You didn`t want to enter Your date of birth!');
+    alert ('So sorry, but You not enter Your year of birth!');
 }
 
-let answerCity;
-// let answerCityLondon;
-// let answerCityWashington;
-// let answerCityElse;
 
+let city = null;
 
 if (userCity === 'Kyiv') {
-    // alert('You live in the capital of Ukraine!');
-    answerCity = 'You live in the capital of Ukraine!';
+    city = 'You live in the capital of Ukraine!';
 } else if (userCity === 'London') {
-    // alert('You live in the capital of United Kingdom!');
-    answerCity = 'You live in the capital of United Kingdom!';
+    city = 'You live in the capital of United Kingdom!';
 } else if (userCity === 'Washington') {
-    // alert('You live in the capital of USA!');
-    answerCity = 'You live in the capital of USA!';
+    city = 'You live in the capital of USA!';
 } else if (userCity === '' || userCity === null) {
-    alert ('So sorry, but You didn`t want to enter Your city!');
+    alert ('So sorry, but You not enter Your city!');
 } else {
-    // alert(`You live in ${userCity}!`);
-    answerCity = `You live in ${userCity}!`;
+    city = `You live in ${userCity}!`;
 }
 
-let answerSport;
-// let answerSportSwimming;
-// let answerSportPoker;
-// let answerSportElse;
+
+let sport = null;
 
 if (userSport === 'Biathlon') {
-    // alert('Cool! Do you want to become a new BOE Johannes?');
-    answerSport = 'Cool! Do you want to become a new BOE Johannes?';
+    sport = 'Cool! Do you want to become a new BOE Johannes?';
 } else if (userSport === 'Swimming') {
-    // alert('Cool! Do you want to become a new Michael Phelps?');
-    answerSport = 'Cool! Do you want to become a new Michael Phelps?';
+    sport = 'Cool! Do you want to become a new Michael Phelps?';
 } else if (userSport === 'Poker') {
-    // alert('Cool! Do you want to become a new Daniel Weinman?');
-    answerSport = 'Cool! Do you want to become a new Daniel Weinman?';
+    sport = 'Cool! Do you want to become a new Daniel Weinman?';
 } else if (userSport === '' || userSport === null) {
-    alert('So sorry, but You didn`t want to enter Your favorite sport!');
+    alert('So sorry, but You not enter Your favorite sport!');
 } else {
-    // alert('Cool! This sport is for special people!');
-    answerSport = 'Cool! This sport is for special people!';
+    sport = 'Cool! This sport is for special people!';
 }
 
-alert(`Your age ${answerAge} years old \n 
-Your city ${answerCity} \n
-Your favorite sport ${answerSport}`)
+
+alert(`Your age ${age} years old, great! \n 
+Your city: ${city} \n
+Your favorite sport: ${sport}`);
