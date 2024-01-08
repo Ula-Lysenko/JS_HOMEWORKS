4d01a8d9aab6de740d05031dd0e5c6f099a528c7
Ula-Lysenko <highspirit@ukr.net>
2024-01-08 16:42:18 +0200

    hw 24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const askNumber = +prompt('Your number?');
+
+if (askNumber < 1) {
+    console.log('NaN');
+}
+
+for (let i = 2; i <= askNumber; i++) {
+    if (askNumber % i === 0) {
+        console.log('min divisor ' + i);
+        break;
+    }
+
+    else if (askNumber % i !== 0) {
+        console.log('prime num ' + askNumber);
+    }
+
+}
+
+
