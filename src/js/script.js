24beeeb569c6d2d87c03e914b1a1cb99fcf05c3f
Ula-Lysenko <highspirit@ukr.net>
2024-01-10 15:08:37 +0200

    hw 19 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// 1
+let x = 10;
+let y = 7;
+
+const result = x > y ? 'X is larger than Y' : 'X is NOT larger than Y';
+console.log(result);
+
+
+// 2
+
+
+// 3
+const numberOne = +prompt('Your first number:');
+
+const numberTwo = +prompt('Your second number:');
+
+const numberThree = +prompt('Your third number:');
+
+if ((numberOne > numberTwo) && (numberOne > numberThree)) {
+  alert(`The FIRST number won! ${numberOne} is the Largest!`);
+} else if ((numberTwo > numberOne) && (numberTwo > numberThree)) {
+  alert(`The SECOND number won! ${numberTwo} is the Largest!`);
+} else if ((numberThree > numberOne) && (numberThree > numberTwo)) {
+  alert(`The THIRD number won! ${numberThree} is the Largest!`);
+} else {
+  alert('Try again!');
+}
+
+
+// 4
