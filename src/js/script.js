6f9c11fe2325dbda215863bd0c3615abca4f50b2
Ula-Lysenko <highspirit@ukr.net>
2024-01-12 16:16:31 +0200

    hw 28 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function padString(string, length, sign, left = false) {
+    if (typeof string !== 'string' || string === '') {
+        return 'Error! Not string!';
+    } else if (typeof length !== 'number' || length === '') {
+        return 'Error! Not length (number)!';
+    } else if (typeof sign !== 'string' || sign === '') {
+        return 'Error! Not sign (string)!';
+    } else if (typeof left !== 'boolean') {
+        return 'Error! Not left (boolean)!';
+    }
+    if (string.length >= length) {
+        return string.slice(0, length);
+    }
+    const emptyInd = length - string.length;
+    const stringNew = left ? sign.repeat(emptyInd) + string : string + sign.repeat(emptyInd);
+    return stringNew;
+}
+
+console.log(padString('Sparkling wine', 35, '*'));
+console.log(padString('Tarantino', 19, '#', true));
