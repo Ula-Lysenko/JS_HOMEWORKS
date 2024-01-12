b0bab63dede21a5164ee9a3f48ec962b4a467fd2
Ula-Lysenko <highspirit@ukr.net>
2024-01-12 13:02:21 +0200

    hw 27 updated

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ const askNumber = +prompt('Your number?');
 const askDegree = +prompt('Your degree?');
 
 function checkNumber(number, degree = 1) {
-    if (isNaN(number) && isNaN(degree)) {
+    if (isNaN(number) || isNaN(degree)) {
         return alert('Error!')
     } else {
         return number ** degree;
