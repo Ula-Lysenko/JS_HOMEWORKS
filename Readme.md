53afb49b044d2453bf4bb3e84932f48af0bb2080
Ula-Lysenko <highspirit@ukr.net>
2024-01-12 22:27:21 +0200

    hw 26 completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
-_Homework description_
+_Реалізуйте функцію generateKey:_
+
+
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters 
+довжиною length.
+
+
+**Наприклад:**
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+
+console.log(key); // eg599gb60q926j8i
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function generateKey(length, characters) {
+    let passwordRandom = '';
+    while (passwordRandom.length < length) {
+        passwordRandom += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return passwordRandom;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(7, characters);
+
+console.log(key);
