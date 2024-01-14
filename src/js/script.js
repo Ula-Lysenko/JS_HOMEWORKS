3024626cf736654fa875d9e85eaa78e33a445000
Ula-Lysenko <highspirit@ukr.net>
2024-01-14 17:19:49 +0200

    hw 30 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const calcCostOfPotatoes = function () {
+    const numOfLiters = 48;
+    const numOfPotatoes = numOfLiters * 4;
+    const weightOfPotatoes = Math.ceil((numOfPotatoes * 75) / 1000);
+    const pricePerKgPotatoes = 13;
+    const totalCostOfPotatoes = weightOfPotatoes * pricePerKgPotatoes;
+
+    return totalCostOfPotatoes;
+}
+
+console.log(calcCostOfPotatoes());
+
