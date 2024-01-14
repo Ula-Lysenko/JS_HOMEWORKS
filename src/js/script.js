c66af1d2c23644f0fc235a8b86d9c8f4817d27fd
Ula-Lysenko <highspirit@ukr.net>
2024-01-14 15:42:21 +0200

    hw 29 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const showPositiveNum = function (array) {
+const arrNew = [];
+if (!array.length) throw new Error(`parameter 'array' is empty`);
+for (let i = 0; i <= array.length; i++) {
+    if (array[i] > 0) arrNew.push(array[i]);
+}
+if (arrNew.length) return arrNew;
+return null;
+}
+
+const arr = [-2, 4, -5, -77, 8, 6];
+console.log(showPositiveNum(arr));
