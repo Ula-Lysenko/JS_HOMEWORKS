810230fa39b7460aef402e21701f3316bc00f7da
Ula-Lysenko <highspirit@ukr.net>
2024-01-16 21:16:58 +0200

    hw 31 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const getNumRandomUniq = function () {
+    const arrRandom = [];
+    let numRandom = null;
+    while (arrRandom.length < 100) {
+       numRandom = Math.ceil(Math.random() * 100);
+       let numFound = false;
+        for (let i = 0; i < arrRandom.length; i++) {
+            if (arrRandom[i] === numRandom) numFound = true;
+        }
+        if (!numFound) arrRandom.push(numRandom);
+    }
+    return arrRandom[numRandom];
+}
+
+console.log(getNumRandomUniq());
