c934c223a1da4fbeb59a29c6e4be9eb7ab40c5ef
Ula-Lysenko <highspirit@ukr.net>
2024-01-17 21:59:08 +0200

    hw 20 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const askNum = +prompt('Число, будь ласка:');
+
+if (isNaN(askNum) || askNum === 0 || askNum === null) alert('Ні-ні! Тільки число!');
+else if (askNum % 10 === 1 && askNum % 100 !== 11) alert(askNum + ' рік');
+else if (askNum % 10 >= 2 && askNum % 10 <= 4 && askNum % 100 !== 12 && askNum % 100 !== 13 && askNum % 100 !== 14 ) alert (askNum + ' роки');
+else alert(askNum + ' років');
+
