fb14766a3dd1a43e7d965a1d2034297c62fbcf91
Ula-Lysenko <highspirit@ukr.net>
2024-01-23 20:11:37 +0200

    hw 30 updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const calcCostOfPotatoes = function () {
-    const numOfLiters = 48;
-    const numOfPotatoes = numOfLiters * 4;
+const calcCostOfPotatoes = function (liters, price) {
+    const numOfPotatoes = liters * 4;
     const weightOfPotatoes = Math.ceil((numOfPotatoes * 75) / 1000);
-    const pricePerKgPotatoes = 13;
-    const totalCostOfPotatoes = weightOfPotatoes * pricePerKgPotatoes;
+    const totalCostOfPotatoes = weightOfPotatoes * price;
 
     return totalCostOfPotatoes;
 }
 
-console.log(calcCostOfPotatoes());
+console.log(calcCostOfPotatoes(48, 13));
 
