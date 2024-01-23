401fa2215dab49532fb0f76897ee76dcc62223ba
Ula-Lysenko <highspirit@ukr.net>
2024-01-23 19:49:51 +0200

    hw 31 updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,16 @@
-'use strict';
+'use strict'
 
+const arrRandom = [];
 const getNumRandomUniq = function () {
-    const arrRandom = [];
-    let numRandom = null;
-    while (arrRandom.length < 100) {
-       numRandom = Math.ceil(Math.random() * 100);
-       let numFound = false;
-        for (let i = 0; i < arrRandom.length; i++) {
-            if (arrRandom[i] === numRandom) numFound = true;
-        }
-        if (!numFound) arrRandom.push(numRandom);
-    }
-    return arrRandom[numRandom];
+    const numRandom = Math.ceil(Math.random() * 100);
+    if (arrRandom.includes(numRandom)) return getNumRandomUniq();
+    arrRandom.push(numRandom);
+    return numRandom;
+}
+
+for (let i = 0; i <= 99; i++) {
+    console.log(getNumRandomUniq());
 }
 
 
-console.log(getNumRandomUniq());
+
