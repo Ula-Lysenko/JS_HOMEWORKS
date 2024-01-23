6e41e55c7e8fd872307f23d19393bc760a3f1654
Ula-Lysenko <highspirit@ukr.net>
2024-01-23 17:50:47 +0200

    hw 34 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-'use strict';
+const createUser = function createUser(name, age) {
+  const user = {
+    name,
+    age,
+    hobby: "hockey",
+  };
+  user.greet = greet;
+  return user;
+};
+
+const greet = function greet() {
+  console.log(
+    `Hi, my name is ${this.name} and I'm ${this.age} years old, and I like ${this.hobby} so much!`,
+  );
+};
+
+const userOne = createUser("Hanna", 37);
+userOne.greet();
+
+const userTwo = createUser("Antonio", 17);
+userTwo.greet();
