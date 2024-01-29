b17236cce9d22439e06e3c33f212104056ee4b2b
Ula-Lysenko <highspirit@ukr.net>
2024-01-29 20:47:36 +0200

    hw 37 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-'use strict';
+const parseUrl = function (url) {
+  const parser = document.createElement("a");
+  parser.href = url;
+
+  const params = {};
+  const query = parser.search.slice(1);
+  const vars = query.split("&");
+  for (let i = 0; i < vars.length; i += 1) {
+    const pair = vars[i].split("=");
+    params[decodeURI(pair[0])] = decodeURIComponent(pair[1]);
+  }
+  return {
+    query,
+    params,
+    origin: parser.origin,
+    protocol: parser.protocol,
+    host: parser.host,
+    path: parser.pathname,
+    hash: parser.hash,
+  };
+};
+
+const url = "https://example.com:8080/path/name?query=123#hash";
+
+console.log(parseUrl(url));
