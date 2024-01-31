eac2902189721c978a14a30b21214fd8f5f3e8f6
Ula-Lysenko <highspirit@ukr.net>
2024-01-31 15:59:14 +0200

    hw 38 updated

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
-_Homework description_
+_Функція Зміни Параметрів URL:_
+
+
+Створіть функцію updateQueryString(url, key, value), яка змінює або додає параметр запиту в URL.
+Функція повинна повертати новий URL з оновленим або доданим параметром запиту.
+
+**https://example.com/search?q=query&sort=ascending&page=2**
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,28 @@
 const updateQueryString = function (url, key, value) {
   const parsedUrl = new URL(url);
-  parsedUrl.searchParams.set(key, value);
+  parsedUrl.searchParams.append(key, value);
 
   return parsedUrl.toString();
 };
 
 console.log(
   updateQueryString(
+    "https://example.com/search?q=query&sort=ascending&page=2",
+    "new illustration",
+    "1.W56",
+  ),
+);
+
+//
+const updateQueryString2 = function (url, key, value) {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set(key, value);
+
+  return parsedUrl.toString();
+};
+
+console.log(
+  updateQueryString2(
     "https://example.com/search?q=query&sort=ascending&page=2",
     "page",
     "113",
