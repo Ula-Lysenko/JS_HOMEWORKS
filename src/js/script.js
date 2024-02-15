3de6d9535f7db0ba9b4f9bf9da22df50884e18e2
Ula-Lysenko <highspirit@ukr.net>
2024-02-15 21:27:39 +0200

    hw 43 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+// task 1
+const getFactorial = function (num) {
+  return num === 1 ? num : num * getFactorial(num - 1);
+};
+
+console.log(getFactorial(4));
+
+// task 2
+const getDegree = function (num, deg) {
+  return deg === 1 ? num : num * getDegree(num, deg - 1);
+};
+
+console.log(getDegree(8, 1));
+
+// task 3
+const getSum = function (a, b) {
+  return b === 0 ? a : getSum(a ^ b, (a & b) << 1);
+};
+
+console.log(getSum(155, 10));
