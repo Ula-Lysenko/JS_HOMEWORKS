c57751826b0d26853d786ea6921cdd0ad9cd17a4
Ula-Lysenko <highspirit@ukr.net>
2024-02-17 19:41:44 +0200

    hw 46 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
-'use strict';
+// реалізація методу
+
+const userAge = +prompt("Age, please?");
+
+const user = {
+  name: "Lola",
+  age: userAge,
+};
+
+const checkAge = function (age) {
+  return age >= 21
+    ? "Congratulations! You can buy it!"
+    : "Sorry! Your age is a TDZ!";
+};
+
+console.log(checkAge.apply(user, [userAge]));
+
+// реалізація функції
+
+const myApply = function (func, context, arrArg) {
+  if (!Array.isArray(arrArg)) {
+    throw new Error("arrArg must be an array");
+  }
+  return func(...arrArg);
+};
+
+const arrArg = [userAge];
+
+console.log(myApply(checkAge, user, arrArg));
