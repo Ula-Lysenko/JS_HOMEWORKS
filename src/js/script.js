2ee587668a024fae4fd15cc7ab446b980877a4af
Ula-Lysenko <highspirit@ukr.net>
2024-02-19 20:06:41 +0200

    hw 51 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+const parseJSONToObj = (jsonStr) => {
+  try {
+    const parsedObject = JSON.parse(jsonStr);
+    return parsedObject;
+  } catch (error) {
+    console.error("Error parsing JSON:", error.message);
+    // return null; // Return null or handle the error as needed
+  }
+};
+
+// Приклад використання:
+const jsonStr = '{"name": "John", "age": , "city": "New York"}';
+console.log(parseJSONToObj(jsonStr));
+// const parsedResult = parseJSONToObj(jsonStr);
+
+// if (parsedResult) {
+//   console.log("Parsed object:", parsedResult);
+// } else {
+//   console.log("Invalid JSON input.");
+// }
