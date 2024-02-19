7d0d11612ff031480ae494398404649014a937e4
Ula-Lysenko <highspirit@ukr.net>
2024-02-19 16:17:19 +0200

    hw 49 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,30 @@
-'use strict';
+// 1
+
+const namesLionKing = ["Simba", "Pumbaa", "Timon", "Rafiki"];
+console.log(namesLionKing);
+
+const longNamesLionKing = namesLionKing.filter((item) => item.length > 5);
+console.log(longNamesLionKing);
+
+// 2
+
+const arrNum = [22, 33, 44, 55, 66];
+console.log(arrNum);
+
+const evenArrNum = arrNum.filter((item) => item % 2 === 0);
+console.log(`even numbers: ${evenArrNum}`);
+
+const sumEvenArrNum = evenArrNum.reduce(
+  (accumulator, currentItem) => accumulator + currentItem,
+  0,
+);
+console.log(`sum of even numbers: ${sumEvenArrNum}`);
+
+// 3
+
+const averageArrNum = arrNum.reduce(
+  (accumulator, currentItem) => accumulator + currentItem,
+  0,
+);
+console.log(`sum of numbers: ${averageArrNum}`);
+console.log(`average of numbers: ${averageArrNum / arrNum.length}`);
