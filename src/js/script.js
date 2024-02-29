f9a9dd81d40ce3573d8cb7c1de309edfde467dad
Ula-Lysenko <highspirit@ukr.net>
2024-02-29 20:06:35 +0200

    hw 52 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+const bodyDoc = document.body;
+const bodyChildren = bodyDoc.children;
+const ul = bodyChildren[0];
+
+for (const elem of ul.children) {
+  console.log(elem);
+}
+
+//
+
+const arrFromUl = Array.from(ul.children);
+console.log(arrFromUl.length);
+
+//
+
+const arrTextContent = [];
+for (let i = 0; i < ul.children.length; i++) {
+  arrTextContent.push(ul.children[i].textContent);
+}
+console.log(arrTextContent);
