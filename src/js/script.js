9a6e2d642659a0334ab6b3582fef674bb8b1d32c
Ula-Lysenko <highspirit@ukr.net>
2024-03-05 19:33:50 +0200

    hw 54 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-'use strict';
+(function () {
+  const generateList = function (array) {
+    const ul = document.createElement("ul");
+
+    for (const item of array) {
+      const li = document.createElement("li");
+
+      if (Array.isArray(item)) {
+        const nestedUl = generateList(item);
+        li.appendChild(nestedUl);
+      } else {
+        li.textContent = item;
+      }
+
+      ul.appendChild(li);
+    }
+
+    return ul;
+  };
+
+  const arrNum = [1, 2, [1.1, 1.2, 1.3, [100, 200]], 3];
+  console.log(arrNum);
+
+  console.log(generateList(arrNum));
+})();
