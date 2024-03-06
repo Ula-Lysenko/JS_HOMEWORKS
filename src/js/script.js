ee1fc1c41a50f0f805a685db315e0053337d595b
Ula-Lysenko <highspirit@ukr.net>
2024-03-06 16:13:14 +0200

    hw 58 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-'use strict';
+const checkNum = function () {
+  const inputElem = document.getElementById("numberUser");
+  const inputValue = parseInt(inputElem.value);
+
+  if (inputValue % 2 === 0) {
+    alert("Ваше число - парне!");
+  } else {
+    alert("Ваше число - непарне!");
+  }
+};
