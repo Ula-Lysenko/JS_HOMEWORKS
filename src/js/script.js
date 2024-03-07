27de12d55cc8e4bfd0686497b1ee3ca17f63eb5d
Ula-Lysenko <highspirit@ukr.net>
2024-03-07 15:27:52 +0200

    hw 59 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-'use strict';
+const createTextArea = function () {
+  const textArea = document.createElement("textarea");
+  textArea.style.margin = "10px";
+  textArea.style.padding = "5px";
+  textArea.style.border = "2px solid #9933FF";
+  const inputElem = document.getElementById("textUser");
+
+  const divElem = document.getElementById("textBlock");
+
+  textArea.innerHTML = inputElem.value;
+  divElem.prepend(textArea);
+  inputElem.value = "";
+};
