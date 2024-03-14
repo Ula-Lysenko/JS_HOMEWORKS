7485638799835e3d99c130dc48081d1d333a2092
Ula-Lysenko <highspirit@ukr.net>
2024-03-14 20:00:44 +0200

    hw 60 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-'use strict';
+(function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nameInp = document.getElementById("name");
+    const emailInp = document.getElementById("email");
+    const passwordInp = document.getElementById("password");
+    const messageElem = document.getElementById("message");
+
+    if (!nameInp.value || !emailInp.value || !passwordInp.value) {
+      messageElem.textContent = "Please, fill in all the fields!";
+    } else {
+      messageElem.textContent = "Registration is successful!";
+    }
+
+    nameInp.value = "";
+    emailInp.value = "";
+    passwordInp.value = "";
+  });
+})();
