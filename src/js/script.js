2e46a2f9baa46b06509ccd7400deb6d9d750b6dc
Ula-Lysenko <highspirit@ukr.net>
2024-03-22 21:27:35 +0200

    hw 62 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,58 @@
-'use strict';
+/* eslint-disable */
+const DATA_KEY = "formData";
+
+(function () {
+  const formPage = () => {
+    const form = document.querySelector("#form");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const inputs = event.target.querySelectorAll("input, select, textarea");
+      const data = {};
+
+      inputs.forEach((input) => {
+        if (input.value.trim() !== "") {
+          data[input.name] = input.value;
+        }
+      });
+
+      if (Object.keys(data).length === 0) {
+        alert("You should fill the form!");
+        return;
+      }
+
+      localStorage.setItem(DATA_KEY, JSON.stringify(data));
+      event.target.reset();
+
+      const url = location.origin + "/JS_HOMEWORKS/src/output.html";
+      window.open(url, "_blank");
+    });
+  };
+
+  const outputPage = () => {
+    const data = JSON.parse(localStorage.getItem(DATA_KEY));
+    const main = document.querySelector("main");
+
+    if (!data) {
+      main.innerHTML = "<h1>No Data!</h1>";
+      return;
+    }
+
+    const ul = document.createElement("ul");
+
+    for (const key in data) {
+      const li = document.createElement("li");
+      li.innerHTML = `<b>${key}: </b> ${data[key]}`;
+      ul.append(li);
+    }
+
+    main.append(ul);
+  };
+
+  if (location.pathname.includes("index.html")) {
+    formPage();
+  } else if (location.pathname.includes("output.html")) {
+    outputPage();
+  }
+})();
